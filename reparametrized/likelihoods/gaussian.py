1a187367fab7acbab205aa7647a768ce9d3e9c05
tkusmierczyk <tomasz.kusmierczyk@gmail.com>
2022-12-01 00:26:30 +0200

    Bugfix MultivariateNormal likelihoods parameters

--- reparametrized/likelihoods/gaussian.py
+++ reparametrized/likelihoods/gaussian.py
@@ -6,7 +6,7 @@ def factorized_gaussian_with_fixed_scale_sample(
     logits: torch.Tensor, n_samples: int = 1, scale: float = 1.0
 ) -> torch.Tensor:
     event_dims = len(logits.shape) - 1
-    d = MultivariateNormal(logits, scale * torch.eye(event_dims))
+    d = MultivariateNormal(logits, scale**2 * torch.eye(event_dims))
     samples = d.rsample(torch.Size([n_samples]))
     return samples
 
@@ -15,6 +15,6 @@ def factorized_gaussian_with_fixed_scale_log_prob(
     logits: torch.Tensor, output_y: torch.Tensor, scale: float = 1.0
 ) -> torch.Tensor:
     event_dims = len(logits.shape) - 1
-    d = MultivariateNormal(logits, scale * torch.eye(event_dims))
+    d = MultivariateNormal(logits, scale**2 * torch.eye(event_dims))
     logliks = d.log_prob(output_y)
     return logliks

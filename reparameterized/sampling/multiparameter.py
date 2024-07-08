84caebab4855ed55851179d98ca09caecb5918dd
tkusmierczyk <tomasz.kusmierczyk@gmail.com>
2024-07-08 09:39:19 +0200

    Add wrapper returning parameters dict for multiparameter sampling (create_multiparameter_sampler_dict)

--- reparameterized/sampling/multiparameter.py
+++ reparameterized/sampling/multiparameter.py
@@ -2,14 +2,14 @@
 
 import torch
 
-from typing import Callable, Iterable, Tuple
+from typing import Callable, Iterable, Tuple, Dict
 
 
 def _multiparameter_sampler_unpack(sampler, parameter2shape):
     """Splits flattened output from sampler into individual parameters according to parameter2shape."""
 
     def wrapped_sampler(n_samples=1):
-        joint_samples, joint_nlls = sampler(n_samples)
+        joint_samples, joint_nll = sampler(n_samples)
 
         samples = []
         start_ix = 0
@@ -26,7 +26,7 @@ def _multiparameter_sampler_unpack(sampler, parameter2shape):
             samples.append(parameter_samples)  # return an ordered list
 
             start_ix += npositions
-        return samples, joint_nlls
+        return samples, joint_nll
 
     return wrapped_sampler
 
@@ -36,7 +36,11 @@ def create_multiparameter_sampler(
     named_parameters: Iterable[Tuple[str, torch.Tensor]],
     **sampler_create_func_args
 ):
-    """ Builds one joint sampler for multiple parameters. """
+    """Builds one joint sampler for multiple parameters.
+
+    Builds a sampler producing unnamed samples ordered accoring to named_parameters.
+    NLL is calculated jointly for all the parameters.
+    """
     named_parameters = list(named_parameters)
 
     # flatten to a single vector:
@@ -51,3 +55,27 @@ def create_multiparameter_sampler(
     parameter2shape = [(n, p.shape) for n, p in named_parameters]
     sampler = _multiparameter_sampler_unpack(sampler, parameter2shape)
     return sampler, variational_params, aux_objs
+
+
+def create_multiparameter_sampler_dict(
+    sampler_create_func: Callable,
+    named_parameters: Dict[str, torch.Tensor],
+    **sampler_create_func_args
+):
+    """Builds one joint sampler for multiple parameters.
+
+    Builds a sampler producing dictionary with named samples: {name [str]: sample [tensor]}.
+    NLL is calculated jointly for all the parameters.
+    """
+    named_parameters = list(named_parameters.items())
+    parameter_names = [n for n, _ in named_parameters]  # extract parameter names
+    sampler, variational_params, aux_objs = create_multiparameter_sampler(
+        sampler_create_func, named_parameters, **sampler_create_func_args
+    )
+
+    def _sampler_dict_wrapper(*args, **kwargs):
+        """Matches unnamed samples from sampler with their names."""
+        parameter_samples, joint_nll = sampler(*args, **kwargs)
+        return dict(zip(parameter_names, parameter_samples)), joint_nll
+
+    return _sampler_dict_wrapper, variational_params, aux_objs

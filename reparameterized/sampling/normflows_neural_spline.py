9a97251d152276d9dddf0ec4be79f9c4372f2ca8
tkusmierczyk <tomasz.kusmierczyk@gmail.com>
2024-09-25 08:58:04 +0200

    Initialize normflows with zeros by default

--- reparameterized/sampling/normflows_neural_spline.py
+++ reparameterized/sampling/normflows_neural_spline.py
@@ -1,6 +1,11 @@
 import normflows as nf
 import logging
-from .normflows_common import NormFlowWrapper, WeightsInitializer, train_nfm
+from .normflows_common import (
+    NormFlowWrapper,
+    WeightsInitializer,
+    train_nfm,
+    ZerosSampler,
+)
 
 
 # Define flows
@@ -14,7 +19,7 @@ def build_spline_flow(
     parameters_shapes=None,
     pretrain_flow=False,
     pretrain_flow_1D_parameters="zeros",  # biases
-    pretrain_flow_2D_parameters="xavier_uniform",  # weights
+    pretrain_flow_2D_parameters="zeros",  # weights
     **layer_args,
 ):
     logging.debug(
@@ -61,14 +66,24 @@ def build_spline_flow(
     nfm.sample(2)
 
     if pretrain_flow:
-        if parameters_shapes is None:
-            raise ValueError("parameters_shapes must be provided if init_flow is True!")
         # Initialize flow to produce parameter samples from some default distributions
-        target = WeightsInitializer(
-            parameters_shapes,
-            weight_init=pretrain_flow_2D_parameters,
-            bias_init=pretrain_flow_1D_parameters,
-        )
+        if (
+            pretrain_flow_1D_parameters == "zeros"
+            and pretrain_flow_2D_parameters == "zeros"
+        ):
+            target = ZerosSampler(output_dim)
+
+        else:
+            if parameters_shapes is None:
+                raise ValueError(
+                    "parameters_shapes must be provided if init_flow is True!"
+                )
+            target = WeightsInitializer(
+                parameters_shapes,
+                weight_init=pretrain_flow_2D_parameters,
+                bias_init=pretrain_flow_1D_parameters,
+            )
+
         train_nfm(nfm, target)
 
     return nfm
--- reparameterized/sampling/normflows_residual.py
+++ reparameterized/sampling/normflows_residual.py
@@ -1,6 +1,11 @@
 import normflows as nf
 import logging
-from .normflows_common import NormFlowWrapper, WeightsInitializer, train_nfm
+from .normflows_common import (
+    NormFlowWrapper,
+    WeightsInitializer,
+    train_nfm,
+    ZerosSampler,
+)
 
 
 # Define flows
@@ -15,7 +20,7 @@ def build_residual(
     parameters_shapes=None,
     pretrain_flow=False,
     pretrain_flow_1D_parameters="zeros",  # biases
-    pretrain_flow_2D_parameters="xavier_uniform",  # weights
+    pretrain_flow_2D_parameters="zeros",  # weights
 ):
     logging.debug(
         f"[build_residual] output_dim={output_dim} "
@@ -51,14 +56,24 @@ def build_residual(
     nfm.sample(2)
 
     if pretrain_flow:
-        if parameters_shapes is None:
-            raise ValueError("parameters_shapes must be provided if init_flow is True!")
         # Initialize flow to produce parameter samples from some default distributions
-        target = WeightsInitializer(
-            parameters_shapes,
-            weight_init=pretrain_flow_2D_parameters,
-            bias_init=pretrain_flow_1D_parameters,
-        )
+        if (
+            pretrain_flow_1D_parameters == "zeros"
+            and pretrain_flow_2D_parameters == "zeros"
+        ):
+            target = ZerosSampler(output_dim)
+
+        else:
+            if parameters_shapes is None:
+                raise ValueError(
+                    "parameters_shapes must be provided if init_flow is True!"
+                )
+            target = WeightsInitializer(
+                parameters_shapes,
+                weight_init=pretrain_flow_2D_parameters,
+                bias_init=pretrain_flow_1D_parameters,
+            )
+
         train_nfm(nfm, target)
 
     return nfm
